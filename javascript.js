d02e8ba7b843268fb3204c9511d2c47853d88b45
yoricktf <yorick.tenfeld@gmail.com>
2021-05-20 16:14:18 +0200

    buttons working, displaying results.

---
+++ javascript.js
@@ -0,0 +1,74 @@
+let 
+// computerWon,
+// userWon,
+computerScore = 0,
+userScore = 0,
+possibleChoice = [1,2,3],
+userchoice,
+userOutput 
+;
+
+//let the computer pick one at random
+function computerChoice (possibleChoice) {
+   possibleChoice = possibleChoice[Math.floor(Math.random() * possibleChoice.length)];
+    if (possibleChoice == 1) {
+        return "rock"
+    } else if (possibleChoice == 2) {
+        return "paper"
+    } else if (possibleChoice == 3) {
+        return "scissors";
+    }
+};     
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button.id)
+        evaluateWinner( computerChoice(possibleChoice), button.id)
+    });
+});
+
+
+//evaluate them against each other including tie
+//output the result, win, loss or tie
+function evaluateWinner( computerChoice, humanChoice) {
+    if 
+        (computerChoice === 'rock' && humanChoice === 'scissors' || 
+            computerChoice === 'paper' && humanChoice === 'rock' || 
+            computerChoice === 'scissors' && humanChoice === 'paper' ) 
+        {
+        document.getElementById('gameMessage').innerHTML = `You lost! ${computerChoice} beats ${humanChoice}`;
+        console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
+        document.getElementById("computerScore").innerHTML = `the computers score is ${computerScore}`;
+        computerScore++;
+        // console.log(`the computers score is ${computerScore}`)
+    } 
+    else if 
+        (humanChoice === 'rock' && computerChoice === 'scissors' ||
+            humanChoice == 'paper' && computerChoice === 'rock' || 
+            humanChoice === 'scissors' && computerChoice === 'paper' ) 
+        {
+        document.getElementById('gameMessage').innerHTML = `You won! ${humanChoice} beats ${computerChoice}`
+        console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+        document.getElementById("userScore").innerHTML = `Your score is ${userScore}`
+        userScore++;
+        // console.log(`Your score is ${userScore}`)
+    } 
+    else {
+        console.log(`call it a draw, you picked ${humanChoice} and the computer picked ${computerChoice}`)
+    } 
+};
+
+// evaluateWinner( computerChoice(possibleChoice), humanChoice())
+
+
+//function to play 5 games 
+//     function fullGame(computerWon,userWon) {    
+// //making the counter 
+//         while (computerScore < 5 && userScore < 5) {
+//             evaluateWinner(computerChoice(possibleChoice),humanChoice());
+//         }
+//     }
+//start the game by calling the function
+// fullGame();
